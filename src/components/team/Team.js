2586ba31a422a02d10a79bd5eedcfdb2324d11b2
Vedantaz <muleyvedant4@gmail.com>
2022-12-01 08:22:30 +0530

    customize to changes

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -197,26 +197,26 @@ const Team = () => {
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://www.linkedin.com/in/prarthana-chandak-2a84b81b7"
+                  href="https://www.linkedin.com/in/abhay-rajput-2a84b81b7"
                 >
                   <i className="fa fa-linkedin ico-link"></i>
                 </a>
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://github.com/prarthanachandak"
+                  href="https://github.com/rajputabhay"
                 >
                   <i className="fa fa-github ico-github"></i>
                 </a>
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://www.instagram.com/prarthanachandak"
+                  href="https://www.instagram.com/rajputabhay"
                 >
                   <i className="fa fa-instagram ico-insta"></i>
                 </a>
                 <a
-                  href="https://github.com/prarthanachandak"
+                  href="https://github.com/rajputabhay"
                   target="_blank"
                   rel="noreferrer"
                 >
